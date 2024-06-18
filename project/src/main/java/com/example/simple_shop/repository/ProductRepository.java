b8daf449a8d70e688d04954eb40fccb9089535d0
Djimi02 <mitko.manolev1@abv.bg>
2024-06-18 18:12:04 +0200

    added forgotten annotations

--- project/src/main/java/com/example/simple_shop/repository/ProductRepository.java
+++ project/src/main/java/com/example/simple_shop/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.example.simple_shop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.simple_shop.entity.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     
 }
--- project/src/main/java/com/example/simple_shop/repository/SubscriberRepository.java
+++ project/src/main/java/com/example/simple_shop/repository/SubscriberRepository.java
@@ -1,9 +1,11 @@
 package com.example.simple_shop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.simple_shop.entity.Subscriber;
 
+@Repository
 public interface SubscriberRepository extends JpaRepository<Subscriber, Long> {
     
 }
