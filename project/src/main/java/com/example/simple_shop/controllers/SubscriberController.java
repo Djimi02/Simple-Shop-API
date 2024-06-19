6b6eb3b219c2b179f0636297a421c4219888715d
Djimi02 <mitko.manolev1@abv.bg>
2024-06-19 18:17:52 +0200

    added subscriber controller

---
+++ project/src/main/java/com/example/simple_shop/controllers/SubscriberController.java
@@ -0,0 +1,56 @@
+package com.example.simple_shop.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.simple_shop.entity.Subscriber;
+import com.example.simple_shop.service.interfaces.SubscriberService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("api/v1/subscribers")
+@AllArgsConstructor
+public class SubscriberController {
+ 
+    private SubscriberService subscriberService;
+
+    @PostMapping("/")
+    public Subscriber saveSubscriber(@RequestBody Subscriber subscriber) {
+        return subscriberService.saveSubscriber(subscriber);
+    }
+
+    @GetMapping("/{subscriberID}")
+    public Subscriber getSubscriberByID(@PathVariable("subscriberID") Long subscriberID) {
+        return subscriberService.getSubscriberByID(subscriberID);
+    }
+
+    @PostMapping("/{subscriberID}/products/{productID}")
+    public void addProductToSubscriber(@PathVariable("subscriberID") Long subscriberID, @PathVariable("productID") Long productID) {
+        subscriberService.addProductToSubscriber(subscriberID, productID);
+    }
+
+    @GetMapping("/{subscriberID}/products")
+    public List<Subscriber> getSubsribersByProductID(Long productID) {
+        return subscriberService.getSubsribersByProductID(productID);
+    }
+
+    @PutMapping("/{subscriberID}")
+    public Subscriber updateSubscriber(@PathVariable("subscriberID") Long subscriberID, @RequestBody Subscriber newSubscriberData) {
+        return subscriberService.updateSubscriber(subscriberID, newSubscriberData);
+    }
+
+    @DeleteMapping("/{subscriberID}")
+    public void deleteSubscriberByID(@PathVariable("subscriberID") Long subscriberID) {
+        subscriberService.deleteSubscriberByID(subscriberID);
+    }
+
+}
